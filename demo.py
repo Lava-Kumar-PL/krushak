6d322e662a5fabf4724f205dbcfdc7e71bc2a5ed
Lava-Kumar-PL <kumar1956.11.1@gmail.com>
2024-12-16 00:20:05 +0530

    Updated README and other files

--- demo.py
+++ demo.py
@@ -1,13 +1,13 @@
 from twilio.rest import Client
 
-account_sid = 'AC964b458a2b4c66e2b0fd9de0ab3a148c'
-auth_token = '6fd33517a1a0983c9cfd992a0eb0f618'
+account_sid = os.getenv("twilio_ssid")
+auth_token = os.getenv("twilio_token")
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
-  from_='whatsapp:+14155238886',
+  from_=os.getenv("twilio_number"),
   body='Hello I am Your Assistant "KrushakAI", How can i Help you Today?',
-  to='whatsapp:+917676815877'
+  to= os.getenv("twilio_toNumber")
 )
 
 print(message.sid)
